232d31b5b7105c17d6b71fdf6e50cf9bb87ee470
Numan Shaikh <shaikhnuman093@gmail.com>
2026-09-07 13:48:35 +0530

    indentent issue solve

--- calculator.py
+++ calculator.py
@@ -8,6 +8,6 @@ def multiply(a, b):
     return a * b 
 
 if __name__ == "__main__": 
-print("2 + 3 =", add(2, 3)) 
-print("10 - 4 =", subtract(10, 4)) 
-print("3 * 5 =", multiply(3, 5)) 
+   print("2 + 3 =", add(2, 3)) 
+   print("10 - 4 =", subtract(10, 4)) 
+   print("3 * 5 =", multiply(3, 5)) 
