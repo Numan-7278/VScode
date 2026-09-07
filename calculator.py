b35122be6d814a3cb8dc27943b6a01beebc79244
Numan Shaikh <shaikhnuman093@gmail.com>
2026-09-07 13:32:28 +0530

    running test fixed

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 def add(a, b): 
-return a + b 
+   return a + b 
+
 def subtract(a, b): 
-return a - b 
+    return a - b 
+
 def multiply(a, b): 
-return a * b 
+    return a * b 
+
 if __name__ == "__main__": 
 print("2 + 3 =", add(2, 3)) 
 print("10 - 4 =", subtract(10, 4)) 
